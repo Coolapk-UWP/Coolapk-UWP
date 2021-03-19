e1a374dc815b7d1c4f7c1fb6ed4935f055fe1524
wherewhere <qq2518766683@outlook.com>
2021-03-19 12:04:36 +0800

    备份

--- src/CoolapkUWP/Pages/MainPage.xaml.cs
+++ src/CoolapkUWP/Pages/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -113,7 +114,7 @@ namespace CoolapkUWP.Pages
             }
         }
 
-        private static void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             UIHelper.RefreshIndexPage();
         }
