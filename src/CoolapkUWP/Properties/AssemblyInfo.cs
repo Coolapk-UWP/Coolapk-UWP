26998ba8bcb87a9c9d0464a654f68551f0eb07c9
wherewhere <qq2518766683@outlook.com>
2021-04-04 23:17:00 +0800

    恢复NuGet包版本(更新之后变卡了。。。

--- src/CoolapkUWP/Properties/AssemblyInfo.cs
+++ src/CoolapkUWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CoolapkUWP")]
-[assembly: AssemblyCopyright("Copyright ©  2017 - 2020")]
+[assembly: AssemblyCopyright("Copyright ©  2017 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
