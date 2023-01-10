f2e203b23ea87aa32ea27e209b85792ba843d91a
wherewhere <qq2518766683@outlook.com>
2023-01-10 19:55:07 +0800

    同步主项目

--- CoolapkUWP/CoolapkUWP/Pages/FeedPages/IndexPage.xaml.cs
+++ CoolapkUWP/CoolapkUWP/Pages/FeedPages/IndexPage.xaml.cs
@@ -7,6 +7,8 @@ using Windows.UI.Xaml.Navigation;
 
 #if FEATURE2
 using GalaSoft.MvvmLight.Command;
+#else
+using CommunityToolkit.Mvvm.Input;
 #endif
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
