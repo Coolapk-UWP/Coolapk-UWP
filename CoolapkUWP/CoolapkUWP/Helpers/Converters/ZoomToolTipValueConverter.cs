22c18002157462c3fe92b440fb6755cb8f1b243d
wherewhere <qq2518766683@outlook.com>
2023-07-24 15:18:53 +0800

    缩放控件提示加入%

---
+++ CoolapkUWP/CoolapkUWP/Helpers/Converters/ZoomToolTipValueConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace CoolapkUWP.Helpers.Converters
+{
+    public class ZoomToolTipValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => ConverterTools.Convert($"{value}%", targetType);
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => ConverterTools.Convert(value.ToString().Replace("%", string.Empty), targetType);
+    }
+}
