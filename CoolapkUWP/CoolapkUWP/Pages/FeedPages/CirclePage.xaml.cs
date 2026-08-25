fe42361ceef9c8d1ceb9a970d78b9d30a6b6cd19
where where <qq2518766683@outlook.com>
2026-08-25 19:30:25 +0800

    补全关注和搜索分类

--- CoolapkUWP/CoolapkUWP/Pages/FeedPages/CirclePage.xaml.cs
+++ CoolapkUWP/CoolapkUWP/Pages/FeedPages/CirclePage.xaml.cs
@@ -58,12 +58,13 @@ namespace CoolapkUWP.Pages.FeedPages
             ResourceLoader loader = ResourceLoader.GetForCurrentView("CirclePage");
             ObservableCollection<PivotItem> items = new ObservableCollection<PivotItem>
             {
-                new PivotItem() { Tag = "V9_HOME_TAB_FOLLOW", Header = loader.GetString("V9_HOME_TAB_FOLLOW"), Content = new Frame() },
-                new PivotItem() { Tag = "circle", Header = loader.GetString("circle"), Content = new Frame() },
-                new PivotItem() { Tag = "apk", Header = loader.GetString("apk"), Content = new Frame() },
-                new PivotItem() { Tag = "topic", Header = loader.GetString("topic"), Content = new Frame() },
-                new PivotItem() { Tag = "question", Header = loader.GetString("question"), Content = new Frame() },
-                new PivotItem() { Tag = "product", Header = loader.GetString("product"), Content = new Frame() }
+                new PivotItem { Tag = "V9_HOME_TAB_FOLLOW", Header = loader.GetString("all"), Content = new Frame() },
+                new PivotItem { Tag = "circle", Header = loader.GetString("circle"), Content = new Frame() },
+                new PivotItem { Tag = "apk", Header = loader.GetString("apk"), Content = new Frame() },
+                new PivotItem { Tag = "topic", Header = loader.GetString("topic"), Content = new Frame() },
+                new PivotItem { Tag = "question", Header = loader.GetString("question"), Content = new Frame() },
+                new PivotItem { Tag = "product", Header = loader.GetString("product"), Content = new Frame() },
+                new PivotItem { Tag = "recent", Header = loader.GetString("recent"), Content = new Frame() }
             };
             return items;
         }
